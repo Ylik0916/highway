5f6d815cae7b90189dacd77cbaacd32eedfb4bcb
gjs <371145877@qq.com>
2023-02-02 17:37:22 +0800

    1

---
+++ highway-common/src/main/java/com/hg/common/enums/DataSourceType.java
@@ -0,0 +1,19 @@
+package com.hg.common.enums;
+
+/**
+ * 数据源
+ * 
+ * @author ruoyi
+ */
+public enum DataSourceType
+{
+    /**
+     * 主库
+     */
+    MASTER,
+
+    /**
+     * 从库
+     */
+    SLAVE
+}
