167760c9a0b58935807a6a548483f120ffc6d984
cqxaj <1956645329@qq.com>
2023-03-21 16:30:50 +0800

    第一版提交前第二次修改完成

--- highway-ui/src/views/system/unit/index.vue
+++ highway-ui/src/views/system/unit/index.vue
@@ -91,7 +91,7 @@
             icon="el-icon-edit"
             @click="handleUpdate(scope.row)"
             v-hasPermi="['system:unit:edit']"
-          >编辑</el-button>
+          >修改</el-button>
           <el-button
             size="mini"
             type="text"
